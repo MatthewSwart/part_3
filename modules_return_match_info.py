16665a99b8c9747a88af4c2efa749850eb4af6de
Matthew Swart <matt.swart@hotmail.com>
2020-11-25 22:59:12 +0000

    Update commit having testing issues

--- modules_return_match_info.py
+++ modules_return_match_info.py
@@ -7,9 +7,13 @@ def return_full_match_info(match_id):
     :param match_id:
     :return:
     """
-    full_match_info = []
+    match_info = []
     match_data = pds()
     for line in match_data:
         if line[0] == match_id:
-            full_match_info.append(line)
-    return full_match_info
+            match_info.append(line)
+    return match_info
+
+if __name__ == '__main__':
+    data = return_full_match_info(203)
+    print(len(data))
--- test_modules_return_match_info.py
+++
@@ -1,6 +0,0 @@
-from modules_return_match_info import f
-
-
-def test_full_match_info():
-    output_data = fmi(203)
-    assert len(output_data) == 233
